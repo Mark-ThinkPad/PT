40662ba20f881a58a319a83dd5c6dc119b13cc28
mark-thinkpad <1372469698@qq.com>
2020-08-03 19:34:58 +0800

    Professional Training

--- example-Django/weather_analysis/weather_analysis/settings.py
+++ example-Django/weather_analysis/weather_analysis/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'w8rid*vfi(z&61xho-e$f#xa3_+#_d6^q4qqx=$fvu^9c_+0uz'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_analysis.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -140,3 +134,8 @@ TX_WEATHER_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/84.0.4147.105 Safari/537.36 '
 }
+
+from datetime import datetime, timedelta
+
+WEATHER_DATE_START = datetime.now() - timedelta(days=1)
+WEATHER_DATE_END = datetime.now() + timedelta(days=6)
--- example-Django/weather_analysis/weather_data/views.py
+++ example-Django/weather_analysis/weather_data/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
 from region.models import Region
 from weather_data.models import WeatherData
 from weather_data.forms import RegionForm
+from util.weather_spider import delete_weather_by_region
+from weather_analysis.settings import WEATHER_DATE_START, WEATHER_DATE_END
 
 
 def region_weather(request, region_id):
@@ -16,8 +19,15 @@ def region_weather(request, region_id):
             region.longitude = form.cleaned_data.get('longitude')
             region.is_display = form.cleaned_data.get('is_display')
             region.save()
+            messages.success(request, '城市信息修改成功!', extra_tags='success')
+        else:
+            messages.error(request, '输入的数据有误, 修改失败!', extra_tags='danger')
         return HttpResponseRedirect(request.path)
 
-    data = WeatherData.objects.filter(region_id=region_id)[:8]
+    data = WeatherData.objects.filter(region_id=region_id,
+                                      time__range=(WEATHER_DATE_START, WEATHER_DATE_END))
+    if data.count() < 8:
+        pass
+
     context = {'region': region, 'data': data, 'region_list': region_list}
     return render(request, 'weather_table.html', context)
