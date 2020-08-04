181ab4302a726427f60969ef57353152f54e14b1
mark-thinkpad <1372469698@qq.com>
2020-08-04 18:36:49 +0800

    Professional Training

--- example-Django/weather_analysis/util/weather_analysis1.py
+++ example-Django/weather_analysis/util/weather_analysis1.py
@@ -5,6 +5,7 @@ from pandas import DataFrame
 import pandas as pd
 
 
+# 获取区域未来六天的天气数据 以列表+字典的形式返回数据
 def get_region_weather_data(region: Region):
     return WeatherData.objects.filter(region=region)\
         .order_by('-created')[:6]\
@@ -12,15 +13,22 @@ def get_region_weather_data(region: Region):
                 'day_wind_power', 'max_degree', 'min_degree')
 
 
+# 获取区域天气数据对应的日期，以列表形式返回数据
 def get_region_weather_date(region: Region) -> list:
     return WeatherData.objects.filter(region=region)\
         .order_by('-created')[:6]\
-        .values_list()
+        .values_list('time')
 
 
 def get_region_weather_dataframe(region: Region) -> DataFrame:
-    pass
+    data = get_region_weather_data(region)
+    date = get_region_weather_date(region)
+    return pd.DataFrame(data, index=date)
 
 
 if __name__ == '__main__':
-    pass
+    region = Region.objects.get(name='武汉市')
+    print(get_region_weather_data(region))
+    print(get_region_weather_date(region))
+    df = get_region_weather_dataframe(region)
+    print(df)
